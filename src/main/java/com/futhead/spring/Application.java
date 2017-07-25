431f709da6876cd3ac53cdb4f996c610ff798e24
futhead <futhead@163.com>
2017-07-25 23:12:55 +0800

    add project

---
+++ src/main/java/com/futhead/spring/Application.java
@@ -0,0 +1,16 @@
+package com.futhead.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Administrator on 2017/7/25.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
