3ca0309c654807aee79b7aead1c4ad93ae826ad5
futhead <futhead@163.com>
2017-07-27 19:05:46 +0800

    Add jdbcTemplate

---
+++ src/main/java/com/futhead/spring/service/UserService.java
@@ -0,0 +1,31 @@
+package com.futhead.spring.service;
+
+/**
+ * Created by futhead on 2017-7-27.
+ */
+public interface UserService {
+
+    /**
+     * 新增一个用户
+     * @param name
+     * @param age
+     */
+    void create(String name, Integer age);
+
+    /**
+     * 根据name删除一个用户
+     * @param name
+     */
+    void deleteByName(String name);
+
+    /**
+     * 获取用户总量
+     */
+    Integer getAllUsers();
+
+    /**
+     * 删除所有用户
+     */
+    void deleteAllUsers();
+
+}
