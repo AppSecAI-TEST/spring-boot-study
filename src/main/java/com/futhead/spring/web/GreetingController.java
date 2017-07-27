ceaffd8e09a097d6c1a65c08f331a0a89e2c8828
futhead <futhead@163.com>
2017-07-27 20:35:24 +0800

    Add global exception

--- src/main/java/com/futhead/spring/web/GreetingController.java
+++ src/main/java/com/futhead/spring/web/GreetingController.java
@@ -2,6 +2,7 @@ package com.futhead.spring.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,10 +10,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/greeting")
+    @GetMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+    @GetMapping("/hello")
+    public String hello() throws Exception {
+        throw new Exception("发生错误");
     }
 
 }
